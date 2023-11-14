c3d48c9f981583cb648e1cce87046146d899321e
hellis <hellisp@gmail.com>
2023-11-14 16:24:36 +0100

    Aggiunta la gestione delle linee spesse

--- src/MainBundle.js
+++ src/MainBundle.js
@@ -1,7 +1,11 @@
 import * as THREE from 'three';
-
+import * as LineMaterial from 'three/addons/lines/LineMaterial.js';
+import * as LineGeometry from 'three/addons/lines/LineGeometry.js';
+import * as Line2 from 'three/addons/lines/Line2.js';
 export { THREE };
+export { LineMaterial };
+export { LineGeometry };
+export { Line2 };
 export { default as proj4 } from 'proj4';
 
 export * from 'Main.js';
-
