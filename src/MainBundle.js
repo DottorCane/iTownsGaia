6e4406a976558c4098d13a050fbd3dcac6cf3531
hellis <hellisp@gmail.com>
2025-01-17 18:01:49 +0100

    fix

--- src/MainBundle.js
+++ src/MainBundle.js
@@ -7,6 +7,7 @@ import * as Line2 from 'three/examples/jsm/lines/Line2.js';
 import * as OrbitControls from 'three/examples/jsm/controls/OrbitControls.js';
 import * as TransformControls from 'three/examples/jsm/controls/TransformControls.js';
 import * as DragControls from 'three/examples/jsm/controls/DragControls.js';
+import * as MeshoptDecoder  from 'three/examples/jsm/libs/meshopt_decoder.module.js';
 
 export { THREE };
 
@@ -16,6 +17,7 @@ export { Line2 };
 export { OrbitControls };
 export { TransformControls };
 export { DragControls };
+export { MeshoptDecoder };
 
 export { default as proj4 } from 'proj4';
 
